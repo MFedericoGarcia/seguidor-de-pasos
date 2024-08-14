00706f853148b00992bf98e6d0a0912902f1898c
Fede Garcia <84251530+MFedericoGarcia@users.noreply.github.com>
2024-08-14 19:32:18 -0300

    created xcode project

---
+++ Seguimiento/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Seguimiento
+//
+//  Created by Fede Garcia on 14/08/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Seguimiento/SeguimientoApp.swift
@@ -0,0 +1,17 @@
+//
+//  SeguimientoApp.swift
+//  Seguimiento
+//
+//  Created by Fede Garcia on 14/08/2024.
+//
+
+import SwiftUI
+
+@main
+struct SeguimientoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
