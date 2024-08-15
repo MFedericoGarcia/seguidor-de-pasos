4897d1bf1edd05d26dee66e034af368f7512bce0
Fede Garcia <84251530+MFedericoGarcia@users.noreply.github.com>
2024-08-15 18:30:16 -0300

    initial navigation setup

--- Seguimiento/ContentView.swift
+++ Seguimiento/ContentView.swift
@@ -7,28 +7,56 @@
 
 import SwiftUI
 
+enum HealthMetricContent: CaseIterable, Identifiable {
+    case steps, weight
+    
+    var id: Self { self }
+    
+    var title: String {
+        switch self {
+        case .steps:
+            return "Pasos"
+        case .weight:
+            return "Peso"
+        }
+    }
+}
+
 struct ContentView: View {
+    
+    @State private var selectedStat: HealthMetricContent = .steps
+    var isSteps: Bool { selectedStat == .steps }
+    
     var body: some View {
        NavigationStack {
             ScrollView {
                 VStack(spacing: 20) {
-                    VStack {
-                        HStack {
-                            VStack(alignment: .leading) {
-                                Label("Pasos", systemImage: "figure.walk")
-                                    .font(.title3.bold())
-                                    .foregroundStyle(.pink)
-                                
-                                Text("Promedio: 10k Pasos")
-                                    .font(.caption)
-                                    .foregroundStyle(.secondary)
-                            }
-                            
-                            Spacer()
-                            
-                            Image(systemName: "chevron.right")
-                                .foregroundStyle(.secondary)
+                    Picker("Estadística", selection: $selectedStat) {
+                        ForEach(HealthMetricContent.allCases) { metric in
+                            Text(metric.title)
                         }
+                    }
+                    .pickerStyle(.segmented)
+                    
+                    VStack {
+                        NavigationLink(value: selectedStat){
+                            HStack {
+                                VStack(alignment: .leading) {
+                                    Label("Pasos", systemImage: "figure.walk")
+                                        .font(.title3.bold())
+                                        .foregroundStyle(.teal)
+                                    
+                                    Text("Promedio: 10k Pasos")
+                                        .font(.caption)
+                                }
+                                        
+                                Spacer()
+                                
+                                Image(systemName: "chevron.right")
+                            }
+                        }
+                        .foregroundStyle(.secondary)
+                        .padding(.bottom, 12)
                         
                         RoundedRectangle(cornerRadius: 12)
                             .foregroundStyle(.secondary)
@@ -41,12 +69,13 @@ struct ContentView: View {
                             VStack(alignment: .leading) {
                                 Label("Promedio", systemImage: "calendar")
                                     .font(.title3.bold())
-                                    .foregroundStyle(.pink)
+                                    .foregroundStyle(.teal)
                                 
                                 Text("Últimos 28 Días")
                                     .font(.caption)
                                     .foregroundStyle(.secondary)
                             }
+                            .padding(.bottom, 12)
                           
                         RoundedRectangle(cornerRadius: 12)
                             .foregroundStyle(.secondary)
@@ -57,12 +86,15 @@ struct ContentView: View {
                 }
                 .padding()
                 .navigationTitle("Dashboard")
-                
+                .navigationDestination(for: HealthMetricContent.self) { metric in
+                    Text(metric.title)
+                }
                 
             }
            
            
         }
+       .tint(isSteps ? .teal : .indigo)
             
     }
 }
